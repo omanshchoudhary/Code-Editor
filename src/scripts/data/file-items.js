51912a695f2f1fc7153d96afd49b9d6086442536
Omansh Choudhary <omansh.choudhary@gmail.com>
2025-08-11 18:31:32 +0530

    Restructure project folders

---
+++ src/scripts/data/file-items.js
@@ -0,0 +1,16 @@
+const files=[
+    {
+        id:1,
+        name: 'index',
+        extension: 'html',
+        status: true
+
+    },
+    {
+        id:2,
+        name: 'main',
+        extension: 'css',
+        status: true
+
+    }
+];
